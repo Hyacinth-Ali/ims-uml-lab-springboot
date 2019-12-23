8b6ce1267acd5ace9afba34df5fb5cf72ecc7f30
Hyacinth Ali <hyacinth.ali@mail.mcgill.ca>
2019-12-23 16:26:35 -0500

    Initial commit

---
+++ IMS/src/main/java/com/ali/hyacinth/ims/model/Employee.java
@@ -0,0 +1,20 @@
+package com.ali.hyacinth.ims.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name = "employees")
+public class Employee {
+	private String userName;
+
+	public void setUserName(String value) {
+		this.userName = value;
+	}
+
+	@Id
+	@GeneratedValue
+	public String getUserName() {
+		return this.userName;
+	}
+}
